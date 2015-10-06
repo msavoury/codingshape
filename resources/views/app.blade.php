00efbe8dbc266485fc0ca153a0a7b6d4e89a1ddb
Marcos Savoury <marcossavoury@hotmail.com>
2015-10-05 23:55:58 -0400

    Adding intro page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,43 @@
-<!doctype html>
-<html>
-<head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
     <title>codingshape.com</title>
-</head>
-<body>
-    @yield('content')
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="/assets/css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="/assets/css/custom.css" media="screen">
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">codingshape.com</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://builtwithbootstrap.com/" target="_blank">Built With Bootstrap</a></li>
+            <li><a href="https://wrapbootstrap.com/?ref=bsw">about</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      @yield('content')
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+          </div>
+        </div>
+      </footer>
+    </div>
+    <script src="/assets/js/lib/jquery-2.1.4.min.js"></script>
+    <script src="/assets/js/lib/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ resources/views/partials/index.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('content')
+<div class="row">
+  <div class="col-lg-12">
+    <div class="bs-component">
+      <div class="jumbotron">
+        <h1 class="text-center" style="font-weight:600">Pump Code.</h1>
+        <p class="text-center">
+            <img height="100" src="/assets/img/dumbbell-512.png">
+        </p>
+        <p class="text-center">keep your coding skills in shape.</p>
+        <p class="text-center"><a href="/workout" class="btn btn-primary btn-lg">Get Started</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
