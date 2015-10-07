fc01d59edbde9bffed173d31d21da6f463fcced6
Marcos Savoury <marcossavoury@hotmail.com>
2015-10-06 20:15:14 -0400

    Adding more to workout template

--- resources/views/partials/index.blade.php
+++ resources/views/partials/index.blade.php
@@ -7,10 +7,10 @@
       <div class="jumbotron">
         <h1 class="text-center" style="font-weight:600">Pump Code.</h1>
         <p class="text-center">
-            <img height="100" src="/assets/img/dumbbell-512.png">
+            <img height="130" src="/assets/img/dumbbell-512.png">
         </p>
-        <p class="text-center">keep your coding skills in shape.</p>
-        <p class="text-center"><a href="/workout" class="btn btn-primary btn-lg">Get Started</a></p>
+        <p class="text-center">Keep your coding skills in shape.</p>
+        <p class="text-center"><a href="/workout" class="btn btn-primary btn-lg">Start Workout</a></p>
       </div>
     </div>
   </div>
--- resources/views/partials/workout.blade.php
+++ resources/views/partials/workout.blade.php
@@ -1,5 +1,16 @@
 @extends('app')
 
 @section('content')
-This is the content of the workout file
+<div class="row">
+  <div class="col-lg-12">
+	Content of the question goes here
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-12">
+        <button class="btn btn-primary btn-lg">Previous</button>
+        <button class="btn btn-primary btn-lg">Flip</button>
+        <button class="btn btn-primary btn-lg">Next</button>
+  </div>
+</div>
 @stop
