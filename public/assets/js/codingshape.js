cc745e365e69884e0aa994e18181e55a18f64c2d
Marcos Savoury <marcossavoury@hotmail.com>
2015-10-13 00:52:29 -0400

    outlining service structure

--- public/assets/js/codingshape.js
+++ public/assets/js/codingshape.js
@@ -2,6 +2,30 @@ var app = angular.module('codingshape', [], function($interpolateProvider) {
      $interpolateProvider.startSymbol('[[');
      $interpolateProvider.endSymbol(']]');
 });
+app.factory('cardService', function() {
+    var cardService = {
+        getCards:function() {
+
+        },
+    };
+    return cardService;
+})
 app.controller('cardController', ['$scope', function($scope) {
     $scope.test = 'hello';
+    /*
+
+       //setup goes here. get cards from the service
+       //vars
+       // - current index in array of cards
+       // - card state: front or back
+       //
+       // card json structure
+       // { front:"front", back:"back"}
+
+
+     */
+    //next btn fn
+    //prv btn fn
+    //flip btn fn
+
 }]);
