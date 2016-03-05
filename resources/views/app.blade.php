06a4118270f3ea0189fc1b3caac626fcd757fff6
Marcos Savoury <marcossavoury@hotmail.com>
2016-03-04 20:57:40 -0500

    Adding about info

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,8 +21,8 @@
         </div>
         <div class="navbar-collapse collapse" id="navbar-main">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="http://builtwithbootstrap.com/" target="_blank">Built With Bootstrap</a></li>
-            <li><a href="https://wrapbootstrap.com/?ref=bsw">about</a></li>
+            <li><a href="">Built With </a></li>
+            <li><a id="about" data-toggle="modal" data-target="#aboutModal" href="">about</a></li>
           </ul>
 
         </div>
@@ -37,6 +37,25 @@
         </div>
       </footer>
     </div>
+    <!--MODALS-->
+    <!-- Modal -->
+<div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">About</h4>
+      </div>
+      <div class="modal-body">
+        Created by <a href="http://www.github.com/msavoury">@marcossavoury</a>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <!--END MODALS -->
     <script src="/assets/js/lib/jquery-2.1.4.min.js"></script>
     <script src="/assets/js/lib/bootstrap.min.js"></script>
     <script src="/assets/js/lib/angular-1.4.5.min.js"> </script>
